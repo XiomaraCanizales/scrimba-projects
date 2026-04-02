5d276f15ccbb3f2e86d20f254e33f7cc87e3af92
Xiomara Canizales <x.xiomara@gmail.com>
2026-04-02 15:00:37 -0600

    adding 3th project

---
+++ 2-frontend-path/3-password-generator/index.js
@@ -0,0 +1,19 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+const pass1 = document.getElementById("pass1")
+const pass2 = document.getElementById("pass2")
+
+function generatePassword() {
+    let password = ""
+    for (let i = 0; i < 15; i++) {
+        let randomCh = Math.floor(Math.random() * characters.length)
+        password += characters[randomCh]
+    }
+    return password
+}
+
+function handleClick() {
+    pass1.textContent = generatePassword()
+    pass2.textContent = generatePassword()
+}
